66fdfd12b34b9122edb1070959de4eeaa442abf3
eetupas <79096066+eetupas@users.noreply.github.com>
2024-10-10 18:28:12 +0300

    Refactor select dropdowns and chart layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './App.css';
 import Plot from 'react-plotly.js';
 
 const GraphComponent = () => {
@@ -32,83 +33,86 @@ const GraphComponent = () => {
 
   return (
     <div>
-      <h1>Plotly Graph from Flask Backend</h1>
-
-      {/* Dropdown for view type (Monthly or Daily) */}
-      <label>View Type:</label>
-      <select value={viewType} onChange={(e) => setViewType(e.target.value)}>
-        <option value="Monthly">Monthly</option>
-        <option value="Daily">Daily</option>
-      </select>
-
-      {/* Dropdown for year */}
-      <label>Year:</label>
-      <select value={year} onChange={(e) => setYear(e.target.value)}>
-        {[2020, 2021, 2022, 2023].map((yr) => (
-          <option key={yr} value={yr}>
-            {yr}
-          </option>
-        ))}
-      </select>
-
-      {/* Dropdown for month */}
-      <label>Month:</label>
-      <select value={month} onChange={(e) => setMonth(e.target.value)}>
-        {[
-          { name: 'January', value: 1 },
-          { name: 'February', value: 2 },
-          { name: 'March', value: 3 },
-          { name: 'April', value: 4 },
-          { name: 'May', value: 5 },
-          { name: 'June', value: 6 },
-          { name: 'July', value: 7 },
-          { name: 'August', value: 8 },
-          { name: 'September', value: 9 },
-          { name: 'October', value: 10 },
-          { name: 'November', value: 11 },
-          { name: 'December', value: 12 },
-        ].map((mnth) => (
-          <option key={mnth.value} value={mnth.value}>
-            {mnth.name}
-          </option>
-        ))}
-      </select>
-
-      {/* Input for day selection, only shown when viewType is Daily */}
-      {viewType === 'Daily' && (
-        <>
-          <label>Day:</label>
-          <input
-            type="number"
-            value={day}
-            onChange={(e) => setDay(e.target.value)}
-            min="1"
-            max="31"
-          />
-        </>
-      )}
-
-      {/* Dropdown for data type */}
-      <label>Data Type:</label>
-      <select value={dataType} onChange={(e) => setDataType(e.target.value)}>
-        {['Temperature', 'Rain', 'Wind', 'Both'].map((type) => (
-          <option key={type} value={type}>
-            {type}
-          </option>
-        ))}
-      </select>
-
-      {/* Button to fetch the graph */}
-      <button onClick={fetchGraph}>Load Graph</button>
-
-      {/* Render the graph using Plotly */}
-      {graphData.length > 0 && (
-        <Plot
-          data={graphData}
-          layout={layout}
-          style={{ width: '100%', height: '100%' }}  // Adjust the size of the graph
-        />
-      )}
+      {/* Flex container for select dropdowns and chart */}
+      <div style={{ borderRadius: '8px', height: '100vh', display: 'flex', justifyContent: 'space-between', margin: '0 15%' }}>      
+        {/* Left side: 1/3 width for dropdowns */}
+        <div style={{borderRadius: '8px', flex: 1, maxHeight: '50%', maxWidth: '33%', background: '#3CB371', padding: '1rem'}}>
+          <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+            <label style={{width: '200px', textAlign: 'center', marginBottom: '1rem', color: '#FFFFFF'}}>View Type:</label>
+            <select style={{width: '200px', marginBottom: '1rem', padding: '0.5rem', border: 'none', borderRadius: '4px', background: '#FFFFFF'}} value={viewType} onChange={(e) => setViewType(e.target.value)}>
+              <option value="Monthly">Monthly</option>
+              <option value="Daily">Daily</option>
+            </select>
+            
+            <label style={{width: '200px', textAlign: 'center', marginBottom: '1rem', color: '#FFFFFF'}}>Select Year</label>
+            <select style={{width: '200px', marginBottom: '1rem', padding: '0.5rem', border: 'none', borderRadius: '4px', background: '#FFFFFF'}} value={year} onChange={(e) => setYear(e.target.value)}>
+              {[2020, 2021, 2022, 2023].map((yr) => (
+                <option key={yr} value={yr}>
+                  {yr}
+                </option>
+              ))}
+            </select>
+  
+            <label style={{width: '200px', textAlign: 'center', marginBottom: '1rem', color: '#FFFFFF'}}>Select Month</label>
+            <select style={{width: '200px', marginBottom: '1rem', padding: '0.5rem', border: 'none', borderRadius: '4px', background: '#FFFFFF'}} value={month} onChange={(e) => setMonth(e.target.value)}>
+              {[
+                { name: 'January', value: 1 },
+                { name: 'February', value: 2 },
+                { name: 'March', value: 3 },
+                { name: 'April', value: 4 },
+                { name: 'May', value: 5 },
+                { name: 'June', value: 6 },
+                { name: 'July', value: 7 },
+                { name: 'August', value: 8 },
+                { name: 'September', value: 9 },
+                { name: 'October', value: 10 },
+                { name: 'November', value: 11 },
+                { name: 'December', value: 12 },
+              ].map((mnth) => (
+                <option key={mnth.value} value={mnth.value}>
+                  {mnth.name}
+                </option>
+              ))}
+            </select>
+  
+            {viewType === 'Daily' && (
+              <>
+                <label style={{width: '200px', textAlign: 'center', marginBottom: '1rem', color: '#FFFFFF'}}>Select Day</label>
+                <input 
+                  style={{width: '200px', marginBottom: '1rem', padding: '0.5rem', border: 'none', borderRadius: '4px', background: '#FFFFFF'}}
+                  type="number"
+                  value={day}
+                  onChange={(e) => setDay(e.target.value)}
+                  min="1"
+                  max="31"
+                />
+              </>
+            )}
+  
+            <label style={{width: '200px', textAlign: 'center', marginBottom: '1rem', color: '#FFFFFF'}}>Select Data Type</label>
+            <select style={{width: '200px', marginBottom: '1rem', padding: '0.5rem', border: 'none', borderRadius: '4px', background: '#FFFFFF'}} value={dataType} onChange={(e) => setDataType(e.target.value)}>
+              {['Temperature', 'Rain', 'Wind', 'Both'].map((type) => (
+                <option key={type} value={type}>
+                  {type}
+                </option>
+              ))}
+            </select>
+  
+            <button onClick={fetchGraph} style={{width: '200px', marginTop: '1rem', padding: '0.5rem 1rem', border: 'none', borderRadius: '4px', backgroundColor: '#FF7F50', color: '#FFFFFF'}}>Load Graph</button>
+          </div>
+        </div>
+        
+        {/* Right side: 2/3 width for chart */}
+        <div style={{ borderRadius: '8px', flex: 2, maxHeight: '50%', maxWidth: '66%',  marginLeft: '8rem', background: 'lightgray'}}>
+          {graphData.length > 0 && (
+            <Plot
+              data={graphData}
+              layout={layout}
+              style={{ width: '100%', height: '100%' }}  // Adjust the size of the graph
+            />
+          )}
+        </div>
+      </div>
     </div>
   );
 };
